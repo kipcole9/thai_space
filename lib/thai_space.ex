5a94f21c2f19a0f497ac06417a059c73d02d732b
Kip Cole <kipcole9@gmail.com>
2024-04-28 22:32:41 +1000

    Initial commit

---
+++ lib/thai_space.ex
@@ -0,0 +1,18 @@
+defmodule ThaiSpace do
+  @moduledoc """
+  Documentation for `ThaiSpace`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThaiSpace.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/thai_space_test.exs
@@ -0,0 +1,8 @@
+defmodule ThaiSpaceTest do
+  use ExUnit.Case
+  doctest ThaiSpace
+
+  test "greets the world" do
+    assert ThaiSpace.hello() == :world
+  end
+end
