6789088b22bbb6fa0005df3ab4310ecbd01d6e7b
Kip Cole <kipcole9@gmail.com>
2024-04-28 23:34:15 +1000

    Add ThaiSpace.string_width/1

--- lib/thai_space.ex
+++ lib/thai_space.ex
@@ -1,18 +1,29 @@
 defmodule ThaiSpace do
   @moduledoc """
-  Documentation for `ThaiSpace`.
+  Thai script calculations
   """
 
   @doc """
-  Hello world.
+  Returns the width a string
+  occupies in the termainl.
 
   ## Examples
 
-      iex> ThaiSpace.hello()
-      :world
+      iex> ThaiSpace.string_width("ผู้นำ")
+      3
 
   """
-  def hello do
-    :world
+  @sara_am 3635
+
+  def string_width(string) when is_binary(string) do
+    string
+    |> String.graphemes()
+    |> Enum.reduce(0, fn grapheme, acc ->
+      case String.to_charlist(grapheme) do
+        [_codepoint_1, @sara_am] -> acc + 2
+        [_codepoint_1, _codepoint_2, @sara_am] -> acc + 2
+        _other -> acc + 1
+      end
+    end)
   end
 end
